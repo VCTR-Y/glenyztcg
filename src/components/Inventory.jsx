b2f959c12df20592c438c9e3bc6be91ca5d370a1
Glenyz <glen.zhu@outlook.com>
2025-03-24 21:37:19 -0700

    Re-order Cards over Packs section

--- src/components/Inventory.jsx
+++ src/components/Inventory.jsx
@@ -19,16 +19,16 @@ function Inventory(props) {
           <li><a href="#cards" className="hover:underline">Cards</a></li>
         </ul>
       </nav>
-      <h2 id="packs" className="text-4xl font-bold text-center mb-4 scroll-mt-42 border-b-1">Packs</h2> {/* Packs Section */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-4 gap-y-8">
-        {packs?.map((item, index) => (
-          <Pack key={index} image={item.image} title={item.title} price={item.price} />
-        ))}
-        </div>
       <h2 id="cards" className="text-4xl font-bold text-center mb-4 scroll-mt-42 border-b-1">Cards</h2> {/* Cards Section */}
       <div className="grid grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-4 gap-y-8">
         {cards?.map((item, index) => (
           <Card key={index} image={item.image} title={item.title} price={item.price} />
+        ))}
+        </div>
+        <h2 id="packs" className="text-4xl font-bold text-center mb-4 scroll-mt-42 border-b-1">Packs</h2> {/* Packs Section */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-4 gap-y-8">
+        {packs?.map((item, index) => (
+          <Pack key={index} image={item.image} title={item.title} price={item.price} />
         ))}
         </div>
     </div>
