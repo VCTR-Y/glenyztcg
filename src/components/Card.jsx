c30db31adcdb8d9949f64a25d2f1428912d53a0d
Victor Ye <vye@students.cs.ubc.ca>
2025-03-26 16:44:06 -0700

    fix grid layout of cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,11 @@
 function Card({ image, title, price }) {
   return (
-    <div className="flex flex-col justify-center items-center p-4 rounded-lg text-center">
-      <img src={image} alt={title} className="w-68 h-96 sm:w-48 sm:h-64 md:w-56 md:h-80 lg:w-68 lg:h-96 object-cover rounded-2xl shadow-[12px_12px_12px_rgba(0,0,0,0.8)] transition-transform duration-300 ease-in-out hover:scale-106 cursor-pointer"/>
-      <span className="text-2xl font-semibold mt-5">{title}</span>
-      <span className="text-xl font-semibold">{price}</span>
+    <div className="flex flex-col justify-start items-center p-4 rounded-lg text-center ">
+      <img src={image} alt={title} className="w-auto h-96 sm:h-64 md:h-80 lg:h-96 object-cover rounded-2xl shadow-[12px_12px_12px_rgba(0,0,0,0.8)] transition-transform duration-300 ease-in-out hover:scale-106 cursor-pointer"/>
+      <div className="flex flex-col items-center">
+        <span className="text-xl font-semibold mt-5">{title}</span>
+        <span className="text-xl font-semibold text-yellow-300">{price}</span>
+      </div>
     </div>
   );
 }
--- src/components/Inventory.jsx
+++ src/components/Inventory.jsx
@@ -15,12 +15,12 @@ function Inventory(props) {
       <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-center mb-4">Inventory</h1>
       <nav className="flex justify-center items-center w-full rounded-lg border-1 py-2 mb-6">
         <ul className="flex space-x-8 text-xl">
-          <li><a href="#packs" className="hover:underline">Packs</a></li>
           <li><a href="#cards" className="hover:underline">Cards</a></li>
+          <li><a href="#packs" className="hover:underline">Packs</a></li>
         </ul>
       </nav>
       <h2 id="cards" className="text-4xl font-bold text-center mb-4 scroll-mt-42 border-b-1">Cards</h2> {/* Cards Section */}
-      <div className="grid grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-4 gap-y-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-4 gap-y-8">
         {cards?.map((item, index) => (
           <Card key={index} image={item.image} title={item.title} price={item.price} />
         ))}
