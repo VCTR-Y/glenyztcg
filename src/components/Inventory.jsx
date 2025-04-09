05bdfdc0ca8691b7c6451b35f638d861754ffca9
victor <victorfengye2004@gmail.com>
2025-04-08 18:34:35 -0700

    implement search

--- src/components/Inventory.jsx
+++ src/components/Inventory.jsx
@@ -1,14 +1,27 @@
+import { useState } from "react";
 import Pack from "./InventoryItems/Pack";
 import Card from "./InventoryItems/Card";
+import searchIcon from "../assets/magnifying-glass-solid.svg";
 
 function Inventory(props) {
-
   const { inventory } = props;
+  const [cardSearchQuery, setCardSearchQuery] = useState("");
+  const [packSearchQuery, setPackSearchQuery] = useState("");
 
   const items = inventory;
 
   const packs = items.filter(item => item.type === "Pack");
   const cards = items.filter(item => item.type === "Card");
+
+  const searchCards = cardSearchQuery.trim() ? 
+    cards.filter((card) => 
+      card.title.toLowerCase().includes(cardSearchQuery.toLowerCase())
+    ) : cards;
+
+  const searchPacks = packSearchQuery.trim() ? 
+  packs.filter((pack) => 
+    pack.title.toLowerCase().includes(packSearchQuery.toLowerCase())
+  ) : packs;
 
   return (
     <div id="inventory" className="w-full max-w-412 mx-auto bg-opacity-50 backdrop-blur-xs rounded-lg shadow-[16px_16px_16px_rgba(0,0,0,0.6)] m-4 p-4 scroll-mt-42 border-2" >
@@ -20,14 +33,38 @@ function Inventory(props) {
         </ul>
       </nav>
       <h2 id="cards" className="text-4xl font-bold text-center mb-4 scroll-mt-42 border-b-1">Cards</h2> {/* Cards Section */}
+      <div className="flex justify-center items-center m-4">
+        <div className="flex justify-center items-center border rounded-lg max-w-xs w-full p-2">
+          <img src={searchIcon} className="h-5 w-5 m-2" />
+          <input 
+            type="text" 
+            placeholder="Search..." 
+            value={cardSearchQuery} 
+            onChange={(e) => setCardSearchQuery(e.target.value)}
+            className="w-full text-lg outline-0 ml-2"
+          />
+        </div>
+      </div>
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-4 gap-y-6">
-        {cards?.map((item, index) => (
+        {searchCards?.map((item, index) => (
           <Card key={index} image={item.image} title={item.title} price={item.price} />
         ))}
         </div>
       <h2 id="packs" className="text-4xl font-bold text-center mb-4 scroll-mt-42 border-b-1 mt-8">Packs</h2> {/* Packs Section */}
+      <div className="flex justify-center items-center m-4">
+        <div className="flex justify-center items-center border rounded-lg max-w-xs w-full p-2">
+          <img src={searchIcon} className="h-5 w-5 m-2" />
+          <input 
+            type="text" 
+            placeholder="Search..." 
+            value={packSearchQuery} 
+            onChange={(e) => setPackSearchQuery(e.target.value)}
+            className="w-full text-lg outline-0 ml-2"
+          />
+        </div>
+      </div>
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-4 gap-y-8">
-        {packs?.map((item, index) => (
+        {searchPacks?.map((item, index) => (
           <Pack key={index} image={item.image} title={item.title} price={item.price} />
         ))}
         </div>
